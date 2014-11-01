b37677ace63d5171af27dd85c8b81ecd2100296d
Matthew Dean <deanmatthew16@gmail.com>
2014-10-31 23:58:30 -0400

    Send private messages

--- src/main/java/net/dean/jraw/managers/InboxManager.java
+++ src/main/java/net/dean/jraw/managers/InboxManager.java
@@ -1,10 +1,12 @@
 package net.dean.jraw.managers;
 
+import net.dean.jraw.ApiException;
 import net.dean.jraw.EndpointImplementation;
 import net.dean.jraw.Endpoints;
 import net.dean.jraw.JrawUtils;
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.http.NetworkException;
+import net.dean.jraw.http.RedditResponse;
 import net.dean.jraw.models.Message;
 import net.dean.jraw.paginators.InboxPaginator;
 
@@ -43,5 +45,46 @@ public class InboxManager extends AbstractManager {
                 .endpoint(read ? Endpoints.READ_MESSAGE : Endpoints.UNREAD_MESSAGE)
                 .post(JrawUtils.args("id", m.getFullName()))
                 .build());
+    }
+
+    /**
+     * Sends a private message as the currently logged in user.
+     *
+     * @param to Who to send this message to
+     * @param subject The message's subject
+     * @param body The message's body
+     * @throws NetworkException If the request did not complete successfully
+     * @throws ApiException If the Reddit API returned an error
+     */
+    public void compose(String to, String subject, String body) throws NetworkException, ApiException {
+        compose("", to, subject, body);
+    }
+
+    /**
+     * Sends a private message.
+     *
+     * @param from Who to send this message as. If sending as the currently authenticated user, leave this empty. If
+     *             sending as a subreddit you moderate, use the name of the subreddit without "/r/" (for example: "pics")
+     * @param to Who to send this message to
+     * @param subject The message's subject
+     * @param body The message's body
+     * @throws NetworkException If the request did not complete successfully
+     * @throws ApiException If the Reddit API returned an error
+     */
+    @EndpointImplementation(Endpoints.COMPOSE)
+    public void compose(String from, String to, String subject, String body) throws NetworkException, ApiException {
+        RedditResponse response = execute(request()
+                .endpoint(Endpoints.COMPOSE)
+                .post(JrawUtils.args(
+                        "api_type", "json",
+                        "from_sr", from,
+                        "subject", subject,
+                        "text", body,
+                        "to", to
+                )).build());
+        if (response.hasErrors()) {
+            throw response.getErrors()[0];
+        }
+        System.out.println();
     }
 }
--- src/test/java/net/dean/jraw/test/auth/InboxManagerTest.java
+++ src/test/java/net/dean/jraw/test/auth/InboxManagerTest.java
@@ -1,11 +1,13 @@
 package net.dean.jraw.test.auth;
 
+import net.dean.jraw.ApiException;
 import net.dean.jraw.http.NetworkException;
 import net.dean.jraw.managers.InboxManager;
 import net.dean.jraw.models.Message;
 import net.dean.jraw.paginators.InboxPaginator;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+
+import static org.testng.Assert.assertEquals;
 
 public class InboxManagerTest extends AuthenticatedRedditTest {
     private InboxManager inbox;
@@ -28,6 +30,17 @@ public class InboxManagerTest extends AuthenticatedRedditTest {
         } catch (IllegalStateException e) {
             // e.getCause() might be a NetworkException
             handle(e.getCause() != null ? e.getCause() : e);
+        }
+    }
+
+    @Test
+    public void testCompose() {
+        try {
+            String subject = getUserAgent(InboxManagerTest.class);
+            String body = "epoch=" + epochMillis();
+            inbox.compose("/r/jraw_testing2", subject, body);
+        } catch (NetworkException | ApiException e) {
+            handle(e);
         }
     }
 
