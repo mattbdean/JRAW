ad46b0a99fb137d5cc6aa9dbfd5d041b12d029f1
PhaniGaddipati <phanigaddipati@gmail.com>
2015-01-22 00:19:22 -0500

    combined ThingMapping tests, clearMap also tested

--- src/test/java/net/dean/jraw/test/ThingManagerTest.java
+++ src/test/java/net/dean/jraw/test/ThingManagerTest.java
@@ -16,31 +16,24 @@ public class ThingManagerTest extends RedditTest {
     private static final String SUBMISSION_ID = "92dd8";
 
     @Test
-    public void testEnabledThingMapping() {
-        ThingManager.get().setEnabled(true);
-
+    public void testThingMap() {
         Submission submission;
         try {
+            ThingManager.get().setEnabled(true);
             submission = reddit.getSubmission(SUBMISSION_ID);
             validateModel(submission);
             String submissionName = submission.getFullName();
             //Test that the found reference matches the expected reference
             Assert.assertEquals(submission, ThingManager.get().getThing(submissionName));
-        } catch (NetworkException e) {
-            handle(e);
-        }
-    }
 
-    @Test
-    public void testDisabledThingMapping() {
-        ThingManager.get().setEnabled(false);
-        ThingManager.get().clearMap();
+            //Test that when disabled/cleared, getThing will return null
 
-        Submission submission;
-        try {
+            ThingManager.get().setEnabled(false);
+            ThingManager.get().clearMap();
+
             submission = reddit.getSubmission(SUBMISSION_ID);
             validateModel(submission);
-            String submissionName = submission.getFullName();
+            submissionName = submission.getFullName();
             //Test that there is no reference
             Assert.assertNull(ThingManager.get().getThing(submissionName));
         } catch (NetworkException e) {
