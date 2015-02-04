0c81a0e3b50ca65ec8ed2cbf10efbff607411678
Matthew Dean <deanmatthew16@gmail.com>
2015-02-03 23:00:45 -0500

    Remove Votable.getUpvotes() and getDownvotes()
    With recent changes in Reddit, getUpvotes() will getScore() and getDownvotes() will always return 0. See this commit:
    
      https://github.com/reddit/reddit/commit/8c9ad4e

--- src/main/java/net/dean/jraw/models/attr/Votable.java
+++ src/main/java/net/dean/jraw/models/attr/Votable.java
@@ -1,7 +1,7 @@
 package net.dean.jraw.models.attr;
 
-import net.dean.jraw.models.meta.JsonProperty;
 import net.dean.jraw.models.VoteDirection;
+import net.dean.jraw.models.meta.JsonProperty;
 
 /**
  * Provides a way to show that this Thing is able to be voted on.
@@ -9,29 +9,6 @@ import net.dean.jraw.models.VoteDirection;
  * @see net.dean.jraw.models.VoteDirection
  */
 public interface Votable extends JsonAttribute {
-
-    /**
-     * Gets the amount of upvotes the object has received
-     *
-     * @return The amount of upvotes the object has received
-     * @deprecated With recent changes in Reddit, this will always be equivalent to {@link #getScore()}. See
-     *             <a href="https://github.com/reddit/reddit/commit/8c9ad4e">this commit</a> for more information.
-     */
-    @JsonProperty
-    @Deprecated
-    public Integer getUpvotes();
-
-    /**
-     * Gets the amount of downvotes the object has received.
-     *
-     * @return The amount of downvotes the post has received
-     * @deprecated With recent changes in Reddit, this will always return 0. See
-     *             <a href="https://github.com/reddit/reddit/commit/8c9ad4e">this commit</a> for more information.
-     */
-    @JsonProperty
-    @Deprecated
-    public Integer getDownvotes();
-
     /**
      * The net score of this Votable model
      *
