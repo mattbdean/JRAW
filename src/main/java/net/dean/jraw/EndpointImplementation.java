e1b523810251d2b7ec7c1ba400cb29c497ce77d2
Matthew Dean <deanmatthew16@gmail.com>
2015-03-22 12:10:13 -0400

    Add to/update some docs

--- src/main/java/net/dean/jraw/EndpointImplementation.java
+++ src/main/java/net/dean/jraw/EndpointImplementation.java
@@ -8,14 +8,15 @@ import java.lang.annotation.ElementType;
 
 /**
  * This annotation shows that this method is ultimately responsible for the implementation of a Reddit API endpoint
- * (such as {@code /api/login})
+ * (such as {@link Endpoints#FLAIRSELECTOR /api/flairselector}). Note that methods that have this annotation
+ * <em>will</em> send at least one HTTP request.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface EndpointImplementation {
     /**
-     * An array of endpoint URIs that this method implements (such as /api/login)
+     * An array of endpoint URIs that this method implements (such as /api/flairselector)
      *
      * @return A list of endpoints
      */
