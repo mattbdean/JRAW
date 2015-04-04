28a57ec544d8179730335f956ed0c905faabb0cc
Matthew Dean <deanmatthew16@gmail.com>
2015-04-04 19:29:15 -0400

    Remove unnecessary 'public' modifiers from interfaces

--- src/main/java/net/dean/jraw/paginators/RedditIterable.java
+++ src/main/java/net/dean/jraw/paginators/RedditIterable.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface RedditIterable<T extends Thing> extends Iterable<Listing<T>> {
     /** Gets the next page */
-    public Listing<T> next() throws NetworkException;
+    Listing<T> next() throws NetworkException;
 
     /**
      * Gets the next page
@@ -20,10 +20,10 @@ public interface RedditIterable<T extends Thing> extends Iterable<Listing<T>> {
      * @throws IllegalStateException If any property was changed after the first listing was requested and
      *                               {@link #reset()} was not called.
      */
-    public Listing<T> next(boolean forceNetwork) throws NetworkException;
+    Listing<T> next(boolean forceNetwork) throws NetworkException;
 
     /** Checks if Reddit can provide a next page. */
-    public boolean hasNext();
+    boolean hasNext();
 
     /**
      * Creates a list of listings whose size is less than or equal to the given number of pages. The amount of time this
@@ -33,7 +33,7 @@ public interface RedditIterable<T extends Thing> extends Iterable<Listing<T>> {
      * @param maxPages The maximum amount of pages to retrieve
      * @return A list of listings
      */
-    public List<Listing<T>> accumulate(int maxPages) throws NetworkException;
+    List<Listing<T>> accumulate(int maxPages) throws NetworkException;
 
     /**
      * Creates a list of Things whose size is less than or equal to
@@ -44,16 +44,16 @@ public interface RedditIterable<T extends Thing> extends Iterable<Listing<T>> {
      * @return A list of Things
      * @throws NetworkException If any request was not successful
      */
-    public List<T> accumulateMerged(int maxPages) throws NetworkException;
+    List<T> accumulateMerged(int maxPages) throws NetworkException;
 
     /** Checks if listings have been iterated yet. */
-    public boolean hasStarted();
+    boolean hasStarted();
 
     /**
      * Resets the RedditIterable; it will begin at page one. Call this method after you change a property relevant to
      * listing retrieval in order to avoid an IllegalStateException from {@link #next(boolean)}
      */
-    public void reset();
+    void reset();
 
     /**
      * Gets the page index, where 1 is the first page and 0 means that a listing has not been requested yet or
@@ -61,11 +61,11 @@ public interface RedditIterable<T extends Thing> extends Iterable<Listing<T>> {
      *
      * @return An integer representing the current page number
      */
-    public int getPageIndex();
+    int getPageIndex();
 
     /**
      * Gets the last page that was retrieved
      * @return The last listing retrieved, or null if this is a new Paginator or {@link #reset()} was just called
      */
-    public Listing<T> getCurrentListing();
+    Listing<T> getCurrentListing();
 }
