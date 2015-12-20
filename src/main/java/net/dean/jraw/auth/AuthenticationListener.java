95ebb8cb04102f5b96ddb70d4df9576a03070988
Matthew Dean <deanmatthew16@gmail.com>
2015-12-19 21:10:17 -0500

    Whoops

--- src/main/java/net/dean/jraw/auth/AuthenticationListener.java
+++ src/main/java/net/dean/jraw/auth/AuthenticationListener.java
@@ -3,7 +3,7 @@ package net.dean.jraw.auth;
 import net.dean.jraw.http.oauth.OAuthData;
 
 /**
- * A listener that is called whenever an AndroidRedditClient is authenticated.
+ * A listener that is called whenever a RedditClient is authenticated.
  */
 public interface AuthenticationListener {
     void onAuthenticated(OAuthData data);
