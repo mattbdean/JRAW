99610faf22c6c22a3c20b4be295651190eb33631
ccrama <ccrama8@gmail.com>
2016-09-01 16:59:28 -0500

    Fixed FullnamesPaginator on small amount of things

--- src/main/java/net/dean/jraw/paginators/FullnamesPaginator.java
+++ src/main/java/net/dean/jraw/paginators/FullnamesPaginator.java
@@ -37,17 +37,22 @@ public class FullnamesPaginator {
 
     /* Override next to utilize RedditClient.get() instead of the default paginator code*/
     public Listing<Thing> next() {
-        String[] toGet = new String[count>fullnames.length?fullnames.length:count];
+        if(count > fullnames.length){
+            index = fullnames.length;
+            return reddit.get(fullnames);
+        } else {
+            String[] toGet = new String[count];
 
-        int target = index + count;
-        if(target > fullnames.length - 1) target = fullnames.length - 1;
-        int current = 0;
-        for(int i = index; i < target; i++){
-            toGet[current] = fullnames[i];
-            current ++;
+            int target = index + count;
+            if (target > fullnames.length - 1) target = fullnames.length - 1;
+            int current = 0;
+            for (int i = index; i < target; i++) {
+                toGet[current] = fullnames[i];
+                current++;
+            }
+            index = target;
+            return reddit.get(toGet);
         }
-        index = target;
-        return reddit.get(toGet);
     }
 
 }
