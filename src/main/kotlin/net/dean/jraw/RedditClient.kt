83c885f3fe4e815e2fe889985f907308cbe1ffb0
Matthew Dean <deanmatthew16@gmail.com>
2017-06-06 17:02:42 -0400

    Add RedditClient.request() methods

--- src/main/kotlin/net/dean/jraw/RedditClient.kt
+++ src/main/kotlin/net/dean/jraw/RedditClient.kt
@@ -19,8 +19,52 @@ class RedditClient(
         .host("oauth.reddit.com")
         .addHeader("Authorization", "bearer ${oauthData.accessToken}")
 
-    fun me(): JsonNode =
-        http.executeSync(requestStub()
-            .path("/api/v1/me")
-            .build()).json
+    /**
+     * Uses the [HttpAdapter] to execute a synchronous HTTP request and returns its JSON value
+     *
+     * ```
+     * val json = reddit.request(reddit.requestStub()
+     *     .path("/api/v1/me")
+     *     .build())
+     * ```
+     */
+    fun request(r: HttpRequest): JsonNode =
+        http.executeSync(r).json
+
+    /**
+     * Adds a little syntactic sugar to the vanilla `request` method.
+     *
+     * The [configure] function will be passed the return value of [requestStub], and that [HttpRequest.Builder] will be
+     * built and send to `request()`. While this may seem a little complex, it's probably easier to understand through
+     * an example:
+     *
+     * ```
+     * val json = reddit.request {
+     *     it.path("/api/v1/foo")
+     *         .header("X-Foo", "Bar")
+     *         .post(mapOf(
+     *             "baz" to "qux"
+     *         ))
+     * }
+     * ```
+     *
+     * This will execute `POST https://oauth.reddit.com/api/v1/foo` with the header 'X-Foo: Bar' and a form body of
+     * `baz=qux`.
+     *
+     * For reference, this same request can be executed like this:
+     *
+     * ```
+     * val json = reddit.request(reddit.requestStub()
+     *     .path("/api/v1/me")
+     *     .addHeader("X-Foo", "Bar")
+     *     .post(mapOf(
+     *         "baz" to "qux"
+     *     )).build())
+     * ```
+     *
+     * @see requestStub
+     */
+    fun request(configure: (stub: HttpRequest.Builder) -> HttpRequest.Builder) = request(configure(requestStub()).build())
+
+    fun me(): JsonNode = request { it.path("/api/v1/me") }
 }
