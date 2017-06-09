4c10c98c57e22a27351c4d50c6b2b68bb518c67b
Matthew Dean <deanmatthew16@gmail.com>
2017-06-09 10:57:08 -0400

    Add documentation for ThingDeserializer

--- lib/src/main/kotlin/net/dean/jraw/databind/ThingDeserializer.kt
+++ lib/src/main/kotlin/net/dean/jraw/databind/ThingDeserializer.kt
@@ -9,6 +9,25 @@ import net.dean.jraw.models.Subreddit
 import net.dean.jraw.models.Thing
 import net.dean.jraw.models.ThingType
 
+/**
+ * This class parses specific data structures from the reddit API into Thing subclasses.
+ *
+ * This deserializer works by examining the structure of the two root nodes of the provided JSON. Any Thing structure
+ * looks something like this:
+ *
+ * ```json
+ * {
+ *   "kind": "...",
+ *   "data": { ... }
+ * }
+ * ```
+ *
+ * where `kind` is a type prefix defined in the [reddit docs](https://www.reddit.com/dev/api/oauth). This class keeps a
+ * mapping of all known type prefixes to their corresponding classes, so when this deserializer encounters JSON with a
+ * `kind` of "t5", it returns a [Subreddit].
+ *
+ * @see ThingType
+ */
 class ThingDeserializer : StdDeserializer<Thing>(Thing::class.java) {
     override fun deserialize(p: JsonParser, ctxt: DeserializationContext?): Thing {
         val mapper = p.codec as ObjectMapper
@@ -16,7 +35,7 @@ class ThingDeserializer : StdDeserializer<Thing>(Thing::class.java) {
 
         val kind = node.get("kind").asText("<no kind property>")
         val type = registry.keys.firstOrNull { it.prefix == kind } ?:
-            throw IllegalArgumentException("No registered type for kind '$kind'")
+            throw IllegalArgumentException("Unknown kind '$kind'")
         val clazz = registry[type]
 
         val dataNode = node.get("data") ?: throw IllegalArgumentException("no data node")
@@ -31,6 +50,9 @@ class ThingDeserializer : StdDeserializer<Thing>(Thing::class.java) {
         )
     }
 
+    /**
+     * A Jackson module that enables the use of [ThingDeserializer].
+     */
     class Module : SimpleModule() {
         init {
             setDeserializerModifier(object: BeanDeserializerModifier() {
