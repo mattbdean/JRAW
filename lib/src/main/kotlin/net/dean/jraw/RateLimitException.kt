cc299f6ba2b65eed45101f0a7eb8fb1a1e74086e
Matthew Dean <deanmatthew16@gmail.com>
2017-06-22 10:15:30 -0400

    Round the cooldown to the nearest second in RateLimitException

--- lib/src/main/kotlin/net/dean/jraw/RateLimitException.kt
+++ lib/src/main/kotlin/net/dean/jraw/RateLimitException.kt
@@ -1,3 +1,4 @@
 package net.dean.jraw
 
-class RateLimitException(val cooldown: Double) : RuntimeException("reddit is ratelimiting this action, try again in $cooldown seconds")
+class RateLimitException(val cooldown: Double) :
+    RuntimeException("reddit is ratelimiting this action, try again in ~${cooldown.toInt()} seconds")
