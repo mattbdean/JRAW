cc29e227044087574ead963ad12d70a5dce8ec47
Matthew Dean <deanmatthew16@gmail.com>
2017-08-21 17:49:03 -0400

    Add MoreChildren docs

--- lib/src/main/kotlin/net/dean/jraw/models/MoreChildren.kt
+++ lib/src/main/kotlin/net/dean/jraw/models/MoreChildren.kt
@@ -3,10 +3,22 @@ package net.dean.jraw.models
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+/**
+ * A MoreChildren is a model used by reddit to represent comments that exist, but could not be presented in the response
+ * due to the large amounts of other, higher priority comments already being shown in the thread. On the website, a
+ * MoreChildren is represented by the text "load more comments (*x* replies)". The average user shouldn't have to deal
+ * with this class directly as [CommentNode] will handle loading more comments for you.
+ *
+ * MoreChildren instances can appear anywhere in a comment tree except at the very root.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class MoreChildren(
+    /** The full name of the comment or submission whose direct, unshown children this model represents. */
     @JsonProperty("parent_id") override val parentFullName: String,
+
+    /** A list of base-36 IDs */
     @JsonProperty("children") val childrenIds: List<String>,
+
     @JsonProperty("name") override val fullName: String,
     override val id: String
 ) : RedditObject(KindConstants.MORE_CHILDREN), NestedIdentifiable {
