9a8f1f5ebc58acc21f30572b7de5bacbf47dbc7a
Matthew Dean <deanmatthew16@gmail.com>
2017-09-01 17:38:34 -0400

    Re-implement InboxReference

---
+++ lib/src/main/java/net/dean/jraw/models/Message.java
@@ -0,0 +1,72 @@
+package net.dean.jraw.models;
+
+import com.google.auto.value.AutoValue;
+import com.squareup.moshi.Json;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import net.dean.jraw.databind.RedditModel;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Date;
+
+@AutoValue
+@RedditModel
+public abstract class Message implements Created, Distinguishable, Identifiable, Votable {
+    /** The full name of the message that kicked off this private message thread, or null if this message isn't a PM */
+    @Nullable
+    @Json(name = "first_message_name") public abstract String getFirstMessage();
+
+    /** The name of the user that created this message */
+    public abstract String getAuthor();
+
+    /** The markdown-formatted message content */
+    public abstract String getBody();
+
+    /** If this message is for a comment, the permalink to said comment with the query "{@code ?context=3}"  */
+    public abstract String getContext();
+
+    @NotNull
+    @Override
+    @Json(name = "created_utc") public abstract Date getCreated();
+
+    /** The name of the user that started the conversation */
+    public abstract String getDest();
+
+    @NotNull
+    @Override
+    @Json(name = "name") public abstract String getFullName();
+
+    /** True if this message represents a reply to a comment made by the logged-in user */
+    @Json(name = "was_comment") public abstract boolean isComment();
+
+    /** If this message has not been marked as read */
+    // we can't use isNew() since 'new' is a reserved keyword
+    @Json(name = "new") public abstract boolean isUnread();
+
+    /** The total amount of comments in the submission, or null if this message is not for a comment */
+    @Nullable
+    public abstract Integer getNumComments();
+
+    /** The full name of the comment or PM that is the predecessor to this one. */
+    @Nullable
+    public abstract String getParentId();
+
+    /** The subject line of the private message, or the string "`comment reply`" for comments. */
+    public abstract String getSubject();
+
+    /**
+     * The name of the subreddit where this comment or username mention was created, or null if this message isn't a
+     * comment or username mention
+     */
+    @Nullable
+    public abstract String getSubreddit();
+
+    @NotNull
+    @Override
+    @Json(name = "likes") public abstract VoteDirection getVote();
+
+    public static JsonAdapter<Message> jsonAdapter(Moshi moshi) {
+        return new AutoValue_Message.MoshiJsonAdapter(moshi);
+    }
+}
--- lib/src/test/kotlin/net/dean/jraw/test/integration/InboxReferenceTest.kt
+++ lib/src/test/kotlin/net/dean/jraw/test/integration/InboxReferenceTest.kt
@@ -1,48 +1,54 @@
 package net.dean.jraw.test.integration
 
-// TODO
-//class InboxReferenceTest : Spek({
-//    val inbox = reddit.me().inbox()
-//
-//    describe("iterate") {
-//        val whereValues = listOf("inbox", "unread", "messages", "comments", "selfreply", "mentions")
-//        for (where in whereValues) {
-//            it("should be able to iterate '$where'") {
-//                val p = inbox.iterate(where).build()
-//                val messages = p.accumulate(maxPages = 2)
-//                // If this assertion is failing when where == "messages", make sure that there is at least one PM from
-//                // an account other than the testing account. For some reason reddit doesn't display any messages if
-//                // they're all sent from the testing user to the testing user.
-//                if (messages.size > 1)
-//                    // Make sure BarebonesPaginator is working correctly as far as dealing with multiple pages
-//                    messages[0][0].fullName.should.not.equal(messages[1][0].fullName)
-//            }
-//        }
-//    }
-//
-//    describe("compose/markRead/delete") {
-//        it("should be able to send a message") {
-//            val body = "random ID: ${randomName()}"
-//            inbox.compose(reddit.requireAuthenticatedUser(), "test PM", body)
-//
-//            // Make sure it appeared in our inbox. Messages sent to yourself are always marked as read, regardless of
-//            // calls to POST /api/unread_message. There's not much we can in this situation besides make sure that the
-//            // response doesn't contain any errors
-//            val message = inbox.iterate("messages").build().next().firstOrNull { it.body == body }
-//            message.should.not.be.`null`
-//
-//            inbox.markRead(false, message!!.fullName)
-//            inbox.markRead(true, message.fullName)
-//
-//            // Jst make sure it doesn't fail
-//            inbox.delete(message.fullName)
-//        }
-//    }
-//
-//    describe("markAllRead") {
-//        it("should mark all messages as read") {
-//            // Again, there's no way we can really test this without involving another testing user
-//            inbox.markAllRead()
-//        }
-//    }
-//})
+import com.winterbe.expekt.should
+import net.dean.jraw.test.TestConfig.reddit
+import net.dean.jraw.test.randomName
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+class InboxReferenceTest : Spek({
+    val inbox = reddit.me().inbox()
+
+    describe("iterate") {
+        val whereValues = listOf("inbox", "unread", "messages", "comments", "selfreply", "mentions")
+        for (where in whereValues) {
+            it("should be able to iterate '$where'") {
+                val p = inbox.iterate(where).build()
+                val messages = p.accumulate(maxPages = 2)
+                // If this assertion is failing when where == "messages", make sure that there is at least one PM from
+                // an account other than the testing account. For some reason reddit doesn't display any messages if
+                // they're all sent from the testing user to the testing user.
+                if (messages.size > 1)
+                    // Make sure BarebonesPaginator is working correctly as far as dealing with multiple pages
+                    messages[0][0].fullName.should.not.equal(messages[1][0].fullName)
+            }
+        }
+    }
+
+    describe("compose/markRead/delete") {
+        it("should be able to send a message") {
+            val body = "random ID: ${randomName()}"
+            inbox.compose(reddit.requireAuthenticatedUser(), "test PM", body)
+
+            // Make sure it appeared in our inbox. Messages sent to yourself are always marked as read, regardless of
+            // calls to POST /api/unread_message. There's not much we can in this situation besides make sure that the
+            // response doesn't contain any errors
+            val message = inbox.iterate("messages").build().next().firstOrNull { it.body == body }
+            message.should.not.be.`null`
+
+            inbox.markRead(false, message!!.fullName)
+            inbox.markRead(true, message.fullName)
+
+            // Jst make sure it doesn't fail
+            inbox.delete(message.fullName)
+        }
+    }
+
+    describe("markAllRead") {
+        it("should mark all messages as read") {
+            // Again, there's no way we can really test this without involving another testing user
+            inbox.markAllRead()
+        }
+    }
+})
