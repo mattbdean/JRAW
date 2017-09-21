0880299019a5b2ed48d09da972c1d07b0c515305
Matthew Dean <deanmatthew16@gmail.com>
2017-09-20 23:15:39 -0400

    Seal UserReference heirarchy

--- lib/src/main/kotlin/net/dean/jraw/references/OtherUserReference.kt
+++
@@ -1,9 +0,0 @@
-package net.dean.jraw.references
-
-import net.dean.jraw.RedditClient
-
-class OtherUserReference(reddit: RedditClient, username: String) : UserReference<OtherUserFlairReference>(reddit, username) {
-    override val isSelf = false
-
-    override fun flairOn(subreddit: String): OtherUserFlairReference = OtherUserFlairReference(reddit, subreddit, username)
-}
