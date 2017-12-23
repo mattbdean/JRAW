4ab56aa85b4e8f3769aa9a9f805735b75ef624ab
Eduard Netsajev <eduard.netsajev93@gmail.com>
2017-12-23 03:18:21 +0200

    javadoc for SimpleFlairInfo

--- lib/src/main/java/net/dean/jraw/models/SimpleFlairInfo.java
+++ lib/src/main/java/net/dean/jraw/models/SimpleFlairInfo.java
@@ -7,17 +7,42 @@ import com.squareup.moshi.Moshi;
 import net.dean.jraw.databind.RedditModel;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
+/**
+ * SimpleFlairInfo contains basic information about user's flair
+ * on a subreddit: username, flair css class, flair text.
+ *
+ * Used by endpoints with `modflair` scope (require moderator priveleges).
+ *
+ * @see net.dean.jraw.references.SubredditReference#flairList()
+ * @see net.dean.jraw.references.SubredditReference#patchFlairList(List<SimpleFlairInfo>)
+ */
 @AutoValue
 @RedditModel(enveloped = false)
 public abstract class SimpleFlairInfo {
 
+    /** Username */
     public abstract String getUser();
 
+    /** CSS class of user's flair used for custom styling of the flair, if any */
     @Nullable
     @Json(name = "flair_css_class") public abstract String getCssClass();
 
+    /** Text displayed on the flair, if any */
     @Nullable
     @Json(name = "flair_text") public abstract String getText();
+
+    /**
+     * Convert this object into a single line in a CSV line used for setting user flairs in bulk.
+     *
+     * @see net.dean.jraw.references.SubredditReference#patchFlairList(List<SimpleFlairInfo>)
+     */
+    public String toCsvLine() {
+        String text = getText() != null ? getText() : "";
+        String cssClass = getCssClass() != null ? getCssClass() : "";
+        return getUser() + "," + text + "," + cssClass;
+    }
 
     public static JsonAdapter<SimpleFlairInfo> jsonAdapter(Moshi moshi) {
         return new AutoValue_SimpleFlairInfo.MoshiJsonAdapter(moshi);
@@ -25,11 +50,5 @@ public abstract class SimpleFlairInfo {
 
     public static SimpleFlairInfo create(String user, String cssClass, String text) {
         return new AutoValue_SimpleFlairInfo(user, cssClass, text);
-    }
-
-    public String toCsvLine() {
-        String text = getText() != null ? getText() : "";
-        String cssClass = getCssClass() != null ? getCssClass() : "";
-        return getUser() + "," + text + "," + cssClass;
     }
 }
