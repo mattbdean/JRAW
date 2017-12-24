a4f66518b536dc1751f2c8446e7fedfb8bd06342
Eduard Netsajev <eduard.netsajev93@gmail.com>
2017-12-24 11:45:55 +0200

    SearchPaginator documentation

--- lib/src/main/kotlin/net/dean/jraw/pagination/SearchPaginator.kt
+++ lib/src/main/kotlin/net/dean/jraw/pagination/SearchPaginator.kt
@@ -9,10 +9,10 @@ import net.dean.jraw.models.Submission
 import net.dean.jraw.models.TimePeriod
 
 /**
- * This class allows access to <a href="https://www.reddit.com/search">Reddit's search functionality</a>.
+ * This class allows access to [Reddit search functionality](https://www.reddit.com/search).
  * Construct the [SearchPaginator] using [SearchPaginator.Builder] constructor or provided methods in its companion object.
  */
-open class SearchPaginator private constructor(
+class SearchPaginator private constructor(
     reddit: RedditClient,
     baseUrl: String,
     val timePeriod: TimePeriod,
@@ -64,8 +64,42 @@ open class SearchPaginator private constructor(
         override fun build(): SearchPaginator = SearchPaginator(reddit, baseUrl, timePeriod, sorting, limit, syntax, query)
     }
 
+    /**
+     * The syntax language of the provided [query]. Reddit's official documentation on different syntaxes is very sparse,
+     * the latest version of the official search documentation doesn't mention the word 'syntax' at all, and the
+     * API documentation just lists the available choices without any explanation.
+     *
+     * See
+     * - [reddit search on Reddit Wiki](https://www.reddit.com/wiki/search)
+     * - [reddit search API documentation](https://www.reddit.com/dev/api#GET_search)
+     *
+     * The last revision of the reddit search wiki page to describe [CLOUDSEARCH] can be found
+     * [here](https://www.reddit.com/wiki/search?v=1d92a164-31a3-11e7-a834-0aab9d436d2a) which was posted around the time
+     * [reddit announced](https://www.reddit.com/r/changelog/comments/694o34/reddit_search_performance_improvements/)
+     * moving from "from the old Amazon CloudSearch domain to a new Amazon CloudSearch domain".
+     */
     enum class QuerySyntax {
-        CLOUDSEARCH, LUCENE, PLAIN
+        /**
+         * The defaul reddit search syntax, also named as "field search" which supports using field queries
+         *
+         * See [Field search](https://www.reddit.com/wiki/search#wiki_field_search)
+         * */
+        LUCENE,
+
+        /**
+         * Compound query language designed by Amazon for developers
+         *
+         * See
+         * - [Constructing Compound Queries](http://docs.aws.amazon.com/cloudsearch/latest/developerguide/searching-compound-queries.html)
+         * - [relevant Reddit wiki revision](https://www.reddit.com/wiki/search?v=1d92a164-31a3-11e7-a834-0aab9d436d2a)
+         * */
+        CLOUDSEARCH,
+
+        /**
+         * Plain text search - no field support, no compund queries. The search engine is looking for posts containing
+         * the [query] text in their titles and self-post texts.
+         */
+        PLAIN
     }
 
     companion object {
