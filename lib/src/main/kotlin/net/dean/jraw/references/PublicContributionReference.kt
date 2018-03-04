42d7f78a08623ed551c005070cb4a7e9d9eef456
Canaan Gifford <canaangifford@gmail.com>
2018-03-04 14:18:54 -0500

    removing unused imports

--- lib/src/main/kotlin/net/dean/jraw/references/PublicContributionReference.kt
+++ lib/src/main/kotlin/net/dean/jraw/references/PublicContributionReference.kt
@@ -2,6 +2,7 @@ package net.dean.jraw.references
 
 import net.dean.jraw.*
 import net.dean.jraw.databind.Enveloped
+import net.dean.jraw.http.HttpResponse
 import net.dean.jraw.models.Comment
 import net.dean.jraw.models.DistinguishedStatus
 import net.dean.jraw.models.VoteDirection
